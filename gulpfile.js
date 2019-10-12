2a62d9966e0db2005b73f47ec2a5a44cc533eff0
solhuang2201 <5l4s06.huang@gmail.com>
2019-10-13 03:10:03 +0800

    save20191013

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 // 引入工具
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
-var autoprefixer = require('autoprefixer');
+var gulp           = require('gulp');
+var $              = require('gulp-load-plugins')();
+var autoprefixer   = require('autoprefixer');
 var mainBowerFiles = require('main-bower-files');
-var browserSync = require('browser-sync').create();
-var minimist = require('minimist')
-var gulpSequence = require('gulp-sequence')
+var browserSync    = require('browser-sync').create();
+var minimist       = require('minimist')
+var gulpSequence   = require('gulp-sequence')
 
 // gulp --env production 壓縮輸出
 var envOption = {
@@ -29,9 +29,13 @@ gulp.task('sass', function () {
     autoprefixer({browsers: ['last 3 version', '>5%', 'ie 8']})
   ];
   return gulp.src('./source/scss/**/*.scss')
+    .pipe($.wait(1000))
   	.pipe($.plumber())
     .pipe($.sourcemaps.init())
-    .pipe($.sass().on('error', $.sass.logError))
+    .pipe($.sass({
+      errLogToConsole: true,
+      outputStyle: 'expanded'
+    }).on('error', $.sass.logError))
     // 編譯完成 css
   	.pipe($.postcss(plugins))
     // 壓縮 css // if it is production
@@ -62,24 +66,51 @@ gulp.task('babel', () => {
 });
 
 // 外部套件
-gulp.task('bower', function() {
-  return gulp.src(mainBowerFiles())
-    .pipe(gulp.dest('./.tmp/vendors'))
-});
+// gulp.task('bower', function() {
+//   gulp.src(mainBowerFiles(['**/*.js']))
+//     .pipe(gulp.dest('./.tmp/vendors/js'))
+
+//   gulp.src(mainBowerFiles(['**/*.css']))
+//     .pipe(gulp.dest('./.tmp/vendors/css'))
+
+//   gulp.src(mainBowerFiles(['/fontawesome/fonts/**.*']))
+//     .pipe(gulp.dest('./.tmp/vendors/fonts'))
+
+//   gulp.src(mainBowerFiles(['/bootstrap-sass/assets/fonts/bootstrap/**.*']))
+//     .pipe(gulp.dest('./.tmp/vendors/fonts'))
+// });
 
 // 套件載入
-gulp.task('vendorsJs', ['bower'], function() {
-  return gulp.src('./.tmp/vendors/**/**.js')
-    .pipe($.order([
-      'jquery.js',
-      'bootstrap.js',
-      '**.js'
-    ]))
-    .pipe($.concat('vendors.js'))
+// gulp.task('vendors', ['bower'], function() {
+//   gulp.src('./.tmp/vendors/js/**.js')    
+//     // 壓縮 // if it is production
+//     .pipe($.if(options.env === 'production', $.uglify()))
+//     .pipe(gulp.dest('./public/js/vendors'))
+
+//   gulp.src('./.tmp/vendors/css/**.css')    
+//     // 壓縮 // if it is production
+//     .pipe($.if(options.env === 'production', $.uglify()))
+//     .pipe(gulp.dest('./public/css/vendors'))
+
+//   gulp.src('./.tmp/vendors/fonts/**.*')    
+//     .pipe(gulp.dest('./public/fonts'))
+// })
+
+gulp.task('vendors', function() {
+
+  gulp.src('./source/vendors/js/**/*.js')
     // 壓縮 js // if it is production
-    .pipe($.if(options.env === 'production', $.uglify()))
-    .pipe(gulp.dest('./public/js'))
-})
+    // .pipe($.if(options.env === 'production', $.uglify()))
+    .pipe(gulp.dest('./public/js/vendors'))
+
+  gulp.src('./source/vendors/css/**/**.css')
+    // 壓縮 // if it is production
+    .pipe($.if(options.env === 'production', $.cleanCss()))
+    .pipe(gulp.dest('./public/css/vendors'))
+
+  gulp.src('./source/fonts/**/**.*')
+    .pipe(gulp.dest('./public/fonts'))
+});
 
 // 建立虛擬空間
 gulp.task('browser-sync', function() {
@@ -92,7 +123,7 @@ gulp.task('browser-sync', function() {
 
 // 圖片壓縮
 gulp.task('imagemin', () =>
-    gulp.src('./source/images/*')
+    gulp.src('./source/images/**/*.*')
         .pipe($.if(options.env === 'production', $.imagemin()))
         .pipe(gulp.dest('./public/images'))
 );
@@ -117,7 +148,7 @@ gulp.task('deploy', function() {
 });
 
 // 完成交付 輸入 gulp bulid --env production
-gulp.task('bulid', gulpSequence('clean', 'jade', 'sass', 'babel', 'vendorsJs'))
+gulp.task('bulid', gulpSequence('clean', 'jade', 'sass', 'babel', 'vendors', 'imagemin'))
 
 // 直接執行 開發模式 gulp
-gulp.task('default', ['jade', 'sass', 'babel', 'vendorsJs', 'browser-sync', 'imagemin', 'watch']);
+gulp.task('default', ['jade', 'sass', 'babel', 'vendors', 'browser-sync', 'imagemin', 'watch']);
--- source/js/all.js
+++ source/js/all.js
@@ -1,12 +1,189 @@
-var header = document.querySelector('header');
-    var headroom = new Headroom(header);
-    headroom.init();
+$(document).ready(function() {
 
-    // menu
-    $('.drawer').drawer();
-    $('[data-submenu]').submenupicker();
-    // wow
-    wow = new WOW({
-        mobile: false
-    })
-    wow.init();
+
+    var $html = $('body,html'),
+        $body = $('body'),
+        $itmMenu = $('.header_menu > li'),
+        $itmMenuA = $('.header_menu > li > a'),
+        $scrollDown = $('.btnScroll'),
+        $ligBox = $('#lightBox'),
+        $ligBoxWrap = $('#lightBoxWrap'),
+        $btnOpen = $('.btn_list'),
+        $btnGoto = $('.btn_goto');
+
+    // element rellax START
+    // var rellax = new Rellax('.rellax');
+    // element rellax END
+    // element paroller START
+    $('.rellax').paroller();
+    // element paroller END
+
+    // img lazy START
+    $('.lazy').Lazy({
+        // effect: 'fadeIn',
+        // effectTime: 100,
+        threshold: 500
+    });
+    // img lazy END
+
+    // img tilt START
+    $('.js-tilt').tilt();
+    // img tilt END
+
+    // WOW START
+    // new WOW().init();
+    // WOW END
+
+    // scroll_down START    
+    $scrollDown.on('click', function(e) {
+        e.preventDefault();
+        $html.stop(false, false).animate({
+            // 
+            scrollTop: $($(this).attr('href')).offset().top + 'px'
+        }, 800);
+        return false;
+    });
+    // scroll_down END
+
+    // header_menu START
+    // Add scrollspy to <body>
+    $body.scrollspy({
+        target: ".navbar",
+        offset: 50
+    });
+
+    // Add smooth scrolling on all links inside the navbar
+    $itmMenuA.on('click', function(event) {
+
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $html.animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function() {
+
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+        });
+    });
+
+    //
+    var box = $(".wheel_point");
+    var boxCenter = [box.offset().left + box.width() / 2, box.offset().top + box.height() / 2];
+
+    $(document).mousemove(function(e) {
+        var angle = Math.atan2(e.pageX - boxCenter[0], -(e.pageY - boxCenter[1])) * (180 / Math.PI);
+
+        box.css({
+            "-webkit-transform": 'rotate(' + angle + 'deg)'
+        });
+        box.css({
+            '-moz-transform': 'rotate(' + angle + 'deg)'
+        });
+        box.css({
+            'transform': 'rotate(' + angle + 'deg)'
+        });
+
+    });
+    //
+
+    // lightBox START
+    var myScroll = new IScroll('#lightBoxList', {
+        mouseWheel: true,
+        click: true,
+        scrollbars: true
+    });
+    function ligBoxClose() {
+        $body.removeClass('overlay');
+        $ligBoxWrap.removeClass('flash slideInUp').addClass('slideOutDown');
+        $ligBox.addClass('closeing');
+    }
+    $btnOpen.on('click', function(e) {
+        e.preventDefault();
+        $body.addClass('overlay');
+        $ligBox.addClass('active in');
+        setTimeout(function() { $ligBoxWrap.addClass('active');myScroll.refresh(); }, 600);
+    });
+    $ligBox.on('click', '.btn_close', function(e) {
+        e.preventDefault();
+        ligBoxClose();
+        setTimeout(function() {
+            $ligBox.removeClass('closeing').removeClass('in').removeClass('active');
+            $ligBoxWrap.removeClass('slideOutDown active').addClass('flash slideInUp');
+        }, 600);
+    });
+    $ligBox.on('click', '.btn_goto', function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        ligBoxClose();
+        setTimeout(function() {
+            $ligBox.removeClass('closeing').removeClass('in').removeClass('active');
+            $ligBoxWrap.removeClass('slideOutDown active').addClass('flash slideInUp');
+            $html.stop(false, false).animate({
+                // 
+                scrollTop: $($this.attr('href')).offset().top + 'px'
+            }, 800);
+            return false;
+        }, 600);
+    });
+    // lightBox END
+
+    var $sec = $('.sec');
+    $(window).on('scroll load', function() {
+        var winH = $(this).height();
+        var scrollT = $(this).scrollTop();
+
+        $sec.each(function(index) {
+            var elemH = $(this).height();
+            var elemT = $(this).position().top;
+
+            if (elemT < scrollT + winH && scrollT < elemT + elemH) {
+                $(this).addClass("active");
+            }
+        });
+    });
+
+    // $(window).on('load resize', function() {
+    //     firefoxResizeCommon();
+    // });
+});
+
+
+// function firefoxResizeCommon() {
+    // 
+    // var sectionCurtainH = $('.sec-curtain'),
+    //     sectionHalfH = $('.sec-half'),
+    //     sectionFullH = $('.sec-fullHeight'),
+    //     mobileFullH = $('.sec-fullHeight-responsive'),
+    //     mobileAutoH = $('.sec-fullHeight.sec-autoHeight-responsive');
+    // var winW = $(window).width(),
+    //     winH = $(window).innerHeight();
+    // var winT = $(window).scrollTop(),
+    //     winH = $(window).innerHeight();
+    // var winB = winT + winH;
+
+    // mobileFullH.height(winH);
+    // mobileAutoH.height(winH);
+    // sectionFullH.height(winH);
+    // sectionCurtainH.height(winH * 0.75);
+    // sectionHalfH.height(winH * 0.5);
+
+    // if (winW < 992 || winW <= 1279 && (window.orientation === 90 || window.orientation === -90)) {
+    //     mobileAutoH.height('auto');
+    // }
+
+    // var $sec = $('.sec');
+    // $sec.each(function() {
+    // var distance = $(this).offset().top + 200;
+    // if (winB >= distance) {
+    // $(this).addClass('active');
+    // } else {
+    //     $(this).removeClass('active');
+    // };
+    // });
+// }
